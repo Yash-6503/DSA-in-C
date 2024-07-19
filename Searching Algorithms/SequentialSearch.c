278b79b29a6e7e46d4c77911a25d46dfad5c38d7
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-19 15:47:19 +0000

    Sequential Search

---
+++ Searching Algorithms/SequentialSearch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Function to perform linear search
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index of the target element
+        }
+    }
+    return -1; // Return -1 if the element is not found
+}
+
+int main() {
+    int size, target, result;
+
+    // Taking input from the user
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    // Calling the linear search function
+    result = linearSearch(arr, size, target);
+
+    // Displaying the result
+    if (result == -1) {
+        printf("Element not found in the array.\n");
+    } else {
+        printf("Element found at index %d.\n", result);
+    }
+
+    return 0;
+}
