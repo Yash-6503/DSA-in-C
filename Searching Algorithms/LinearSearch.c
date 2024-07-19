14046c690dc265f7edc22171e4621aba4c2002c4
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-19 12:55:55 +0000

    Linear Search Algorithm

--- Searching Algorithms/LinearSearch.c
+++ Searching Algorithms/LinearSearch.c
@@ -1,4 +1,32 @@
 #include<stdio.h>
 int main()
 {
+    int key, n, a[10];
+    int i, found = 0;
+    printf("\nEnter how many elements: ");
+    scanf("%d", &n);
+    printf("\nEnter %d elements: ", n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nEnter Key to search: ");
+    scanf("%d", &key);
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i] == key)
+        {
+            printf("\nElement found at %d position\n", i+1);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("\nElement not found\n");
+    }
+    return 0;
 }
